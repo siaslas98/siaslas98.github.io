93a402836793d47247c2ccee1c6e7597dd82f172
siaslas <huang.treanthony@ufl.edu>
2025-12-09 01:40:35 -0500

    make about section more responsive

--- script.js
+++ script.js
@@ -1,27 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("contactForm");
-  const thankYou = document.getElementById("thankYouMessage");
-
-  form.addEventListener("submit", async function (e) {
-    e.preventDefault();
-    const formData = new FormData(form);
-
-    await fetch(form.action, {
-      method: "POST",
-      body: formData,
-      headers: {
-        Accept: "application/json",
-      },
-    });
-
-    form.reset();
-    thankYou.style.display = "block";
-  });
-});
-
-const toggle = document.getElementById("menu-toggle");
-const nav = document.getElementById("navbar");
-
-toggle.addEventListener("click", () => {
-  nav.classList.toggle("active");
-});
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
