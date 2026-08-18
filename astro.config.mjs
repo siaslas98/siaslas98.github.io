c49b77723326d4913605cfc792016ca4dcc055e6
siaslas <huang.treanthony@ufl.edu>
2026-08-17 23:44:50 -0400

    clean up and remove blog setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://siaslas98.github.io',
   base: '/',
+    devToolbar: {
+        enabled: false,
+    },
 });
--- public/script.js
+++ public/script.js
@@ -4,3 +4,23 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+let lastScrollY = window.scrollY;
+const header = document.querySelector("header");
+
+window.addEventListener(
+    "scroll",
+    () => {
+        const currentScrollY = window.scrollY;
+
+        if (currentScrollY <= 0){
+            header.classList.remove("hidden");
+        } else if (currentScrollY > lastScrollY){
+            header.classList.add("hidden");
+        } else {
+            header.classList.remove("hidden");
+        }
+
+        lastScrollY = currentScrollY;
+    }, {passive: true}
+);
